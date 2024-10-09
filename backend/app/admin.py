3d6666f2c6c709e15dce4af15fa955b394d8e043
victor ngeno <ngenovictor321@gmail.com>
2024-10-09 16:22:15 +0300

    add models to the backend

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Animal, AnimalBreed, AnimalPurchase, AnimalSale, AnimalType
+
+
+@admin.register(AnimalType)
+class AnimalTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AnimalBreed)
+class AnimalBreedAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AnimalPurchase)
+class AnimalPurchaseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AnimalSale)
+class AnimalSaleAdmin(admin.ModelAdmin):
+    pass
